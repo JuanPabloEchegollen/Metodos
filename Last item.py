29c2aed1fb2bbb111aa7049abb236df80da87b89
JuanPabloEchegollen <42146906+JuanPabloEchegollen@users.noreply.github.com>
2018-08-09 08:36:26 -0500

    Update Last item.py

--- Last item.py
+++ Last item.py
@@ -1,4 +1,4 @@
-Create a function that accepts a list and returns the last item in the list. The list can be either homogeneous or heterogeneous.
+"Create a function that accepts a list and returns the last item in the list. The list can be either homogeneous or heterogeneous."
 
 def getLastItem(lst):
   return lst[-1]
